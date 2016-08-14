e22cc04449bbda173e8e0ec911fd4221e0d3518c
James Nylen <jnylen@gmail.com>
2016-08-14 01:26:07 -0500

    Rewrite sparkles to use more RAM but be easier to understand and modify
    Also make exploding sparkles kill other ones nearby

--- lib/mode_Sparkles.h
+++ lib/mode_Sparkles.h
@@ -1,18 +1,13 @@
-u16 sparkleCounter = 0;
-u16 sparkleIndex = 1;
+#define NUM_SPARKLES 30
+u16 sparklePositions[NUM_SPARKLES];
+u8 sparkleFrames[NUM_SPARKLES];
 
-// This is a complicated animation; we can save memory by using a pseudorandom
-// sequence and "replaying" the necessary part of it during each loop
-
-// https://oeis.org/A246866
-#define XOR_MASK_512 0x0240
-
-#define SPARKLE_FRAMES 30 // also the number of sparkles
+#define SPARKLE_FRAMES 50
 
 // Animation transition points by frame index
 #define SPARKLE_END_FADE        8
-#define SPARKLE_END_RAINBOW     22
-#define SPARKLE_END_DESATURATE  26
+#define SPARKLE_END_RAINBOW     42
+#define SPARKLE_END_DESATURATE  46
 #define SPARKLE_END_EXPLODE     SPARKLE_FRAMES
 
 #define SPARKLE_FRAMES_DESATURATE (SPARKLE_END_DESATURATE - SPARKLE_END_RAINBOW)
@@ -24,78 +19,88 @@ void mode_Sparkles_setup() {
 
 void mode_Sparkles_activate() {
 	// Restart the sequence with black each time
-	sparkleCounter = 0;
-	// Set a random starting point (XOR sequence doesn't work with 0)
-	do {
-		sparkleIndex = random16() % NUM_LEDS;
-	} while (sparkleIndex == 0);
+	for (u8 i = 0; i < NUM_SPARKLES; i++) {
+		sparklePositions[i] = NUM_LEDS;
+	}
 }
 
 void mode_Sparkles_loop() {
 	setAll(CRGB::Black);
 
-	u16 sparkleIndexTemp = sparkleIndex;
+	for (u8 i = 0; i < NUM_SPARKLES; i++) {
+		u16 pos = sparklePositions[i];
+		if (pos < NUM_LEDS) {
+			u8 frame = sparkleFrames[i];
 
-	u16 start = max(sparkleCounter, SPARKLE_FRAMES) - SPARKLE_FRAMES;
-
-	for (u16 i = start; i <= sparkleCounter; i++) {
-		u16 ledIndex = (sparkleIndexTemp + randOffset) % NUM_LEDS;
-
-		// Frame index of this sparkle
-		u8 frame = sparkleCounter - i;
-
-		CRGB color = CHSV((ledIndex + frame) * 5, 255, 255);
-		if (frame < SPARKLE_END_FADE) {
-			// beginning; fade in
-			color = blend(
-				CRGB::Black,
-				color,
-				frame * (255 / SPARKLE_END_FADE)
-			);
-			setLed(ledIndex, color);
-		} else if (frame < SPARKLE_END_RAINBOW) {
-			// middle; rainbow shifting
-			setLed(ledIndex, color);
-		} else if (frame < SPARKLE_END_DESATURATE) {
-			// before explosion effect; fade to white
-			color = blend(
-				white,
-				color,
-				(
-					(SPARKLE_END_DESATURATE - frame) *
-					(255 / SPARKLE_FRAMES_DESATURATE)
-				)
-			);
-			setLed(ledIndex, color);
-		} else {
-			// end; explosion effect
-			u8 distance =
-				SPARKLE_FRAMES_EXPLODE -
-				(SPARKLE_END_EXPLODE - frame);
-			color = blend(
-				white,
-				CRGB::Black,
-				distance * (255 / SPARKLE_FRAMES_EXPLODE)
-			);
-			u16 edge1 = max(ledIndex, distance) - distance;
-			u16 edge2 = min(ledIndex, NUM_LEDS - distance) + distance;
-			setLed(edge1, color);
-			for (u16 j = edge1 + 1; j < edge2; j++) {
-				setLed(j, CRGB::Black);
+			CRGB color = CHSV((pos + frame) * 5, 255, 255);
+			if (frame < SPARKLE_END_FADE) {
+				// beginning; fade in
+				color = blend(
+					CRGB::Black,
+					color,
+					frame * (255 / SPARKLE_END_FADE)
+				);
+				setLed(pos, color);
+			} else if (frame < SPARKLE_END_RAINBOW) {
+				// middle; rainbow shifting
+				setLed(pos, color);
+			} else if (frame < SPARKLE_END_DESATURATE) {
+				// before explosion effect; fade to white
+				color = blend(
+					white,
+					color,
+					(
+						(SPARKLE_END_DESATURATE - frame) *
+						(255 / SPARKLE_FRAMES_DESATURATE)
+					)
+				);
+				setLed(pos, color);
+			} else {
+				// end; explosion effect
+				if (frame == SPARKLE_END_EXPLODE - SPARKLE_FRAMES_EXPLODE) {
+					// Explosions kill all nearby sparkles
+					for (u8 j = 0; j < NUM_SPARKLES; j++) {
+						u16 pos2 = sparklePositions[j];
+						if (
+							j != i &&
+							pos2 + SPARKLE_FRAMES_EXPLODE >= pos &&
+							pos2 <= pos + SPARKLE_FRAMES_EXPLODE
+						) {
+							sparklePositions[j] = NUM_LEDS;
+						}
+					}
+				}
+				u8 distance =
+					SPARKLE_FRAMES_EXPLODE -
+					(SPARKLE_END_EXPLODE - frame);
+				color = blend(
+					white,
+					CRGB::Black,
+					distance * (255 / SPARKLE_FRAMES_EXPLODE)
+				);
+				u16 edge1 = max(pos, distance) - distance;
+				u16 edge2 = min(pos, NUM_LEDS - distance) + distance;
+				setLed(edge1, color);
+				for (u16 j = edge1 + 1; j < edge2; j++) {
+					setLed(j, CRGB::Black);
+				}
+				setLed(edge2, color);
 			}
-			setLed(edge2, color);
-		}
 
-		sparkleIndexTemp =
-			(sparkleIndexTemp >> 1) ^
-			(sparkleIndexTemp & 1 ? XOR_MASK_512 : 0);
+			if (++sparkleFrames[i] == SPARKLE_FRAMES) {
+				sparklePositions[i] = NUM_LEDS;
+			}
+		}
 	}
 
-	sparkleCounter++;
-	if (sparkleCounter >= SPARKLE_FRAMES) {
-		sparkleIndex =
-			(sparkleIndex >> 1) ^
-			(sparkleIndex & 1 ? XOR_MASK_512 : 0);
+	if (random8() < 32) {
+		for (u8 i = 0; i < NUM_SPARKLES; i++) {
+			if (sparklePositions[i] == NUM_LEDS) {
+				sparklePositions[i] = random16() % NUM_LEDS;
+				sparkleFrames[i] = 0;
+				break;
+			}
+		}
 	}
 
 	frame(20);
