3e1390915dccb56b23de5d91931598423f3a12f3
James Nylen <jnylen@gmail.com>
2016-08-14 19:20:13 -0500

    Move color definitions to a separate file

---
+++ lib/colors.h
@@ -0,0 +1,9 @@
+// Colors for Sky background
+CRGB green = CRGB(100, 255,  0);
+CRGB blue  = CRGB(  0, 150, 80);
+CRGB white = CRGB(200, 150, 50);
+
+// Colors for hit animation
+CRGB red    = CRGB(255,   0,   0);
+CRGB yellow = CRGB(200, 150,   0);
+CRGB orange = CRGB(225,  50,   0);
